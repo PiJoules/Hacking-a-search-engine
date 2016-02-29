6d8bc4580b94bbf7cda7c68174c63d2392fc13b5
pijoules <lchan1994@yahoo.com>
2016-02-28 20:35:18 -0500

    gotta increase that github score

--- hack.pyx
+++ hack.pyx
@@ -1,77 +1,8 @@
 from __future__ import print_function
 
-import re
-import string
-
-from itertools import combinations
-
-cdef DEL_CHARS = "".join(c for c in map(chr, range(256))
-                         if not c.isalnum())
-
-cdef set all_substrings(char* string, unsigned int length=1):
-    """Get all substrings of a string."""
-    # Initialization
-    cdef set substrs = set()
-    cdef unsigned int slen = len(string)
-
-    # Logic
-    for i in xrange(slen + 1 - length):
-        substr = string[i:i + length]
-        if substr not in substrs:
-            substrs.add(substr)
-
-    # Return
-    return substrs
-
-
-cdef char* simplify(s):
-    """Remove all non alphanumeric chars."""
-    return s.translate(None, DEL_CHARS).lower()
-
-
-cdef set common_substrings(tuple str_list, unsigned int length=1):
-    """Get list of common substrings in each string in str_list."""
-    # Initialization
-    cdef set substrs = all_substrings(simplify(str_list[0]), length=length)
-    cdef char* string
-
-    # Logic
-    for i in xrange(1, len(str_list)):
-        string = simplify(str_list[i])
-        substrs = substrs & all_substrings(string, length=length)
-
-    # Return
-    return substrs
-
-
-cpdef set fewest_queries(list results, unsigned int length=1):
-    """Return queries that result in getting all results."""
-    # Initialization
-    cdef set queries = set()
-    cdef set common = set()
-    cdef set found = set()  # Results already found
-    cdef set combo_set = set()
-
-    # Logic
-    for i in xrange(len(results), 0, -1):
-        for combination in combinations(results, i):
-            common = common_substrings(combination, length=length)
-            # Check if any substrs found
-            if common:
-                # Check if the combination contains results we have not
-                # yet found.
-                combo_set = set(combination)
-                if combo_set - found:
-                    found.update(combo_set)
-                    queries.add(common.pop())  # Only need 1 common substr
-                    if len(found) == len(results):
-                        return queries
-
-    # Return (None found)
-    return set()
-
-
 from collections import defaultdict
+
+
 cdef substrs(set lines, int length):
     """Generate dict of each substring combination to the line it came from."""
     # Initialization
@@ -91,22 +22,38 @@ cpdef list queries(list lines, int length=1):
     # Initialization
     cdef list queries = []
     cdef char* word
+    cdef set remove
     cdef set unique_lines = set([filter(str.isalnum, x).lower() for x in lines])
     cdef common = substrs(unique_lines, length)
+
+    # Sort by longest sentence to shortest sentence.
+    # Sort this way since lambda functions aren't yest supported
+    # in cython.
     cdef list prefixes = sorted([[-len(sentences), substr, sentences]
-                                  for substr, sentences in common.iteritems()])
+                                 for substr, sentences in common.iteritems()])
 
     # Logic
     for i in xrange(len(prefixes)):
+        # Skip ones that we have seen before
         if not(unique_lines & prefixes[i][2]):
             continue
+
+        # Get the substring
         word = prefixes[i][1]
-        to_remove = set([j for j in unique_lines if word in j])
-        unique_lines.difference_update(to_remove)
+
+        # Do not use filter here because closures are not yet supported
+        remove = set([j for j in unique_lines if word in j])
+
+        # Remove the sentences visited
+        unique_lines.difference_update(remove)
         for p in prefixes:
-            p[2].difference_update(to_remove)
+            p[2].difference_update(remove)
             p[0] = -len(p[2])
+
+        # Sort by sentence length again
         prefixes[i + 1:] = sorted(prefixes[i + 1:])
+
+        # Add the new word
         queries.append(word)
 
     # Return
